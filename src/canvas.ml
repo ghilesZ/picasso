179d8bf9a68b60cf8d350e29ca7a871109963b91
ghilesZ <ghiles.ziat@gmail.com>
2020-12-08 11:24:41 +0100

    ocamlformatting

--- src/canvas.ml
+++ src/canvas.ml
@@ -1,6 +1,5 @@
 open Tools
-
-module Draw = Drawer.Make(Gtkcanvas)
+module Draw = Drawer.Make (Gtkcanvas)
 
 class canvas ~packing ~width ~height () =
   let hbox = GPack.hbox ~width ~height ~packing () in
@@ -10,37 +9,32 @@ class canvas ~packing ~width ~height () =
     val mutable rend = None
 
     method get_render () =
-      match rend with
-      | None -> failwith "render not set"
-      | Some r -> !r
+      match rend with None -> failwith "render not set" | Some r -> !r
 
-    method set_render (render:Rendering.t ref) =
-      rend <- Some render;
+    method set_render (render : Rendering.t ref) =
+      rend <- Some render ;
       self#mouse_set
-        ~expose:
-        (fun (a,b) ->
-          render := !render |> Rendering.change_size (float a) (float b);
-          self#repaint())
-        ~drag:
-         (fun (a,b) (x,y) ->
-           render := Rendering.translate (x -. a, y -. b) (!render);
-           self#repaint ();
-         )
-        ~scrollwheel:(fun direction ->
-          render :=
-            (match direction with
-             | `DOWN ->  Rendering.zoom (!render)
-             | `UP ->  Rendering.unzoom (!render)
-             | _ -> !render);
+        ~expose:(fun (a, b) ->
+          render := !render |> Rendering.change_size (float a) (float b) ;
           self#repaint ())
-      ()
+        ~drag:(fun (a, b) (x, y) ->
+          render := Rendering.translate (x -. a, y -. b) !render ;
+          self#repaint ())
+        ~scrollwheel:(fun direction ->
+          (render :=
+             match direction with
+             | `DOWN -> Rendering.zoom !render
+             | `UP -> Rendering.unzoom !render
+             | _ -> !render) ;
+          self#repaint ())
+        ()
 
     (* Repaint the widget. *)
     method repaint () =
-      let drawable = self#get_drawable() in
-      Gtkcanvas.set_drawable drawable;
-      Draw.clear();
-      Draw.draw (self#get_render());
+      let drawable = self#get_drawable () in
+      Gtkcanvas.set_drawable drawable ;
+      Draw.clear () ;
+      Draw.draw (self#get_render ()) ;
       ()
   end
 
@@ -55,9 +49,8 @@ let build render =
   let height = render.window.sy |> iof in
   let title = Option.value render.window.title ~default:"Picasso" in
   let window = GWindow.window ~width ~height ~title () in
-  window#connect#destroy ~callback:GMain.Main.quit |> ignore;
-  window#event#add ([`ALL_EVENTS]);
+  window#connect#destroy ~callback:GMain.Main.quit |> ignore ;
+  window#event#add [`ALL_EVENTS] ;
   let canvas = create_canvas ~packing:window#add ~height ~width in
   let render = ref render in
-  canvas#set_render render;
-  window
+  canvas#set_render render ; window
--- src/picasso.ml
+++ src/picasso.ml
@@ -1,22 +1,17 @@
 module Colors = Colors
-
 module Rendering = Rendering
-
 module Rendering3d = Rendering3d
-
 module Drawable = Drawable
 
 let in_gtk_canvas render =
-  GtkMain.Main.init () |> ignore;
+  GtkMain.Main.init () |> ignore ;
   let window = Canvas.build render in
-  window#show ();
-  GMain.Main.main ()
+  window#show () ; GMain.Main.main ()
 
-let to_latex ?tikz_only:(t=true) render output =
-  Texcanvas.tikz_only := t;
+let to_latex ?tikz_only:(t = true) render output =
+  Texcanvas.tikz_only := t ;
   Tex.output render output
 
-let to_svg render output =
-  Svg.output render output
+let to_svg render output = Svg.output render output
 
 let to_obj render output = Obj.output render output
