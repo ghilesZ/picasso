f16af4b157bd89228fefb98f67fe9454d2f94692
ghilesZ <ghiles.ziat@gmail.com>
2020-12-08 11:43:47 +0100

    code cleaning

--- src/backend.ml
+++ src/backend.ml
@@ -2,25 +2,29 @@ open Geometry
 
 (* backend functions required to be able to draw abstract elements *)
 module type T = sig
-
   type color
 
   (* backend specific function to be called at the end of execution *)
   val ending : unit -> unit
 
-  val width : unit -> int
-  val height : unit -> int
+  val width : unit -> float
+
+  val height : unit -> float
 
   val normalize : range -> range -> point -> point
+
   val rgb : int -> int -> int -> color
 
-  val draw_text : color -> [<`Center|`Left |`Right ] -> point -> string -> unit
+  val draw_text :
+    color -> [< `Center | `Left | `Right] -> point -> string -> unit
+
   val draw_line : color -> point -> point -> unit
 
   val draw_circle : color -> point -> float -> unit
+
   val fill_circle : color -> point -> float -> unit
 
   val draw_poly : color -> point list -> unit
-  val fill_poly : color -> point list -> unit
 
+  val fill_poly : color -> point list -> unit
 end
