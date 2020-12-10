6408d7e9a9690165a7fe5b1543e994ae8ab4c8fd
ghilesZ <ghiles.ziat@gmail.com>
2020-12-10 12:13:00 +0100

    add variable name in axis

--- src/drawer.ml
+++ src/drawer.ml
@@ -107,41 +107,41 @@ module Make (D : Backend.T) = struct
     Tools.iterate fx left (( +. ) step_w) (( < ) right) ;
     Tools.iterate fy down (( +. ) step_h) (( < ) up)
 
-  let draw_axes render =
+  let draw_axes r =
     let open Rendering in
     let pad = 30. in
     let x0, y0 = (0., 0.) in
-    let left = render.scene.x_min and right = render.scene.x_max in
-    let up = render.scene.y_max and down = render.scene.y_min in
-    let hx, hy = normalize render (left, y0)
-    and hx', hy' = normalize render (right, y0) in
+    let left = r.scene.x_min and right = r.scene.x_max in
+    let up = r.scene.y_max and down = r.scene.y_min in
+    let hx, hy = normalize r (left, y0)
+    and hx', hy' = normalize r (right, y0) in
     let th = 2. in
     let thick_line =
       [(hx, hy +. th); (hx, hy -. th); (hx', hy' -. th); (hx', hy' +. th)]
     in
     fill_poly gray (List.rev_map (fun (x, y) -> (x, y)) thick_line) ;
-    let vx, vy = normalize render (x0, down)
-    and vx', vy' = normalize render (x0, up) in
+    let vx, vy = normalize r (x0, down)
+    and vx', vy' = normalize r (x0, up) in
     let thick_line =
       [(vx +. th, vy); (vx -. th, vy); (vx' -. th, vy'); (vx' +. th, vy')]
     in
     fill_poly gray (List.rev_map (fun (x, y) -> (x, y)) thick_line) ;
     let minibar_size = 6. in
     let fx cur =
-      let x, _ = normalize render (cur, down) in
+      let x, _ = normalize r (cur, down) in
       draw_line gray (x, hy -. minibar_size) (x, hy +. minibar_size)
     in
     let fy cur =
-      let _, y = normalize render (left, cur) in
+      let _, y = normalize r (left, cur) in
       draw_line gray (vx -. minibar_size, y) (vx +. minibar_size, y)
     in
-    graduation 0.5 fx fy render ;
+    graduation 0.5 fx fy r ;
     (* vetrtical coordinates *)
     let fx =
       let flag = ref 0 in
       fun cur ->
         let text = Format.asprintf "%a" Tools.pp_float cur in
-        let x, _ = normalize render (cur, down) in
+        let x, _ = normalize r (cur, down) in
         if !flag mod 4 = 0 then draw_text darkgray `Center (x, pad) text ;
         incr flag
     in
@@ -150,21 +150,23 @@ module Make (D : Backend.T) = struct
       let flag = ref 0 in
       fun cur ->
         let text = Format.asprintf "%a" Tools.pp_float cur in
-        let _, y = normalize render (left, cur) in
+        let _, y = normalize r (left, cur) in
         if !flag mod 2 = 0 then draw_text darkgray `Center (pad, y) text ;
         incr flag
     in
-    graduation 0.5 fx fy render
+    graduation 0.5 fx fy r ;
+    draw_text black `Center (r.window.sx /. 2., pad /. 2.) r.abciss ;
+    draw_text black `Center (pad /. 2., r.window.sy /. 2.) r.ordinate
 
   (* main drawing function *)
-  let draw render =
+  let draw r =
     let open Rendering in
-    x_min := render.scene.x_min ;
-    x_max := render.scene.x_max ;
-    y_min := render.scene.y_min ;
-    y_max := render.scene.y_max ;
-    render |> to_vertices
+    x_min := r.scene.x_min ;
+    x_max := r.scene.x_max ;
+    y_min := r.scene.y_min ;
+    y_max := r.scene.y_max ;
+    r |> to_vertices
     |> List.iter (fun ((r, g, b), e) -> polygon (D.rgb r g b) e) ;
-    if render.grid then draw_grid render ;
-    if render.axis then draw_axes render
+    if r.grid then draw_grid r ;
+    if r.axis then draw_axes r
 end
